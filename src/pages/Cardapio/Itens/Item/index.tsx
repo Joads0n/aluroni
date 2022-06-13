5e50581d998050b1c76adfb8b9e7036637a31455
Joadson Silva <joadson83@hotmail.com>
2022-06-13 14:47:05 -0300

    Filtro

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,37 +1,36 @@
 import itemStyles from './Item.module.scss';
-import logo from 'assets/logo.svg';
+import cardapio from '../itens.json';
+import classNames from 'classnames';
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(props : Props) {
+    const { title, description, category, size, serving, price, photo} = props;
     return (
         <div className={itemStyles.item}>
 
             <div className={itemStyles.item__imagem}>
-                <img src={logo} alt="imagem" />
+                <img src={photo} alt={title} />
             </div>
 
             <div className={itemStyles.item__descricao}>
                 
                 <div className={itemStyles.item__titulo}>
-                    <h2>Macarao</h2>
-                    <p>Descriçao macarão</p>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
                 </div>
 
                 <div className={itemStyles.item__tags}>
-                    <div className={itemStyles.item__tipo}>
-                       Massa 
+                    <div className={classNames({
+                        [itemStyles.item__tipo]: true,
+                        [itemStyles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+                    })}
+                    >
+                        {category.label}
                     </div>
-
-                    <div className={itemStyles.item__porcao}>
-                       400g 
-                    </div>
-
-                    <div className={itemStyles.item__qtdpessoas}>
-                       Serve 2 pessoas 
-                    </div>
-
-                    <div className={itemStyles.item__valor}>
-                       R$ 50,00 
-                    </div>
+                    <div className={itemStyles.item__porcao}>{size}g</div>
+                    <div className={itemStyles.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
+                    <div className={itemStyles.item__valor}>R$ {price.toFixed(2)}</div>
                 </div>
 
             </div>
