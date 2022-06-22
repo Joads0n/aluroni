c3705603025cb827c5361221b482a02d7eef31ec
Joadson Silva <joadson83@hotmail.com>
2022-06-22 17:35:33 -0300

    página Sobre

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,7 +1,38 @@
 import sobreStyles from './Sobre.module.scss';
+import temaStyles from 'styles/Tema.module.scss';
+import casa from 'assets/sobre/casa.png';
+import massa1 from 'assets/sobre/massa1.png';
+import massa2 from 'assets/sobre/massa2.png';
+
+const imagens = [massa1, massa2];
 
 export default function Sobre() {
   return (
-    <section>Sobre</section>
+    <section>
+      <h3 className={temaStyles.titulo}>Sobre</h3>
+      <div className={sobreStyles.sobreNos}>
+        <img src={casa} alt="Casa Aluroni" />
+        <div className={sobreStyles.sobreNos__texto}>
+          <p>
+            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
+          </p>
+          <p>
+            Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto!
+          </p>
+          <p>
+            Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, que harmonizam perfeitamente com o seu parto, seja carne ou massa!
+          </p>
+        </div>
+      </div>
+      <div className={sobreStyles.imagens}>
+        {imagens.map((imagem, index) => (
+          <div key={index} 
+            className={sobreStyles.imagens__imagem}
+          >
+            <img src={imagem} alt="imagem da massa" />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
